9967ed26ee44227d2d741d0ada81b3f0ca7db4f1
prajwal kumar <62468512+prajwal-creator@users.noreply.github.com>
2021-03-11 15:00:39 +0530

    customized dice simulator
    1st dice shows the no. 6 with high probability.

--- dice_simulator.py
+++ dice_simulator.py
@@ -9,10 +9,11 @@ label = tkinter.Label(root, text='', font=('Helvetica', 260))
 
 def roll_dice():
     dice = ['\u2680', '\u2681', '\u2682', '\u2683', '\u2684', '\u2685']
-    label.config(text=f'{random.choice(dice)} {random.choice(dice)}')
+    dice1 = ['\u2680', '\u2681', '\u2682', '\u2683', '\u2684', '\u2685','\u2685','\u2685','\u2685']
+    label.config(text=f'{random.choice(dice1)} {random.choice(dice)}')
     label.pack()
 
 button = tkinter.Button(root, text='roll dice', foreground='blue', command=roll_dice)
-button.place(x=265,y=450)   
+button.place(x=265,y=450)
 button.pack()
 root.mainloop()
