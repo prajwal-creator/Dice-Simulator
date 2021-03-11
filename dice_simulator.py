e845a2c638b51fb09c7b1c4af7470012af1f34b0
prajwal kumar <62468512+prajwal-creator@users.noreply.github.com>
2021-03-11 14:08:45 +0530

    Add files via upload

---
+++ dice_simulator.py
@@ -0,0 +1,18 @@
+import tkinter
+import random
+
+root = tkinter.Tk()
+root.geometry('600x600')
+root.title('Roll Dice')
+
+label = tkinter.Label(root, text='', font=('Helvetica', 260))
+
+def roll_dice():
+    dice = ['\u2680', '\u2681', '\u2682', '\u2683', '\u2684', '\u2685']
+    label.config(text=f'{random.choice(dice)} {random.choice(dice)}')
+    label.pack()
+
+button = tkinter.Button(root, text='roll dice', foreground='blue', command=roll_dice)
+button.place(x=265,y=450)   
+button.pack()
+root.mainloop()
